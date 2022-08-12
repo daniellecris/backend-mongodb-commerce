5b24dee0c519c57ac09c23c8fb54d3e8b3ca876c
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-08-12 19:58:12 -0300

    refactor: correção lint

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,6 +1,7 @@
 db.produtos.find({
   $or: [
-    { curtidas: { $eq: 36 }},
-    { vendidos: { $eq: 85 }}
+    { curtidas: { $eq: 36 } },
+    { vendidos: { $eq: 85 } }
   ] },
-  { nome: true, curtidas: true, vendidos: true, _id: false });
+  { nome: true, curtidas: true, vendidos: true, _id: false }
+);
