9d15a7ee5a3c63e2e6fa2ed821e953e7dc3c8e42
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-08-12 19:39:06 -0300

    feat: nomes e curtidas dos lanches - desafio 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,4 +3,4 @@ db.produtos.find({
     { curtidas: { $eq: 36 }},
     { vendidos: { $eq: 85 }}
   ] },
-  { nome: 1, curtidas: 1, vendidos: 1, _id: false });
+  { nome: true, curtidas: true, vendidos: true, _id: false });
