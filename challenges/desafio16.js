c84a967aed5366522869a93125df798a2a47a959
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-08-16 12:11:26 -0300

    feat: desafio 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,6 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+);
+
+db.produtos.find({ ultimaModificacao:{ $exists: 1 } }, { nome: true, _id: false });
