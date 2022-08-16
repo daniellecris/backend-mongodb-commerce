9f5f92d06136c7f31c383471274630e61883c60b
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-08-16 12:11:59 -0300

    feat: desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,6 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.count(),
+});
+
+db.resumoProdutos.find({}, { franquia: true, totalProdutos: true, _id: false });
